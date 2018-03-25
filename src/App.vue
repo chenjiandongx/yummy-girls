03f136be063ac2395e2f3903b3d6f92774f42ebc
chenjiandongx <chenjiandongx@qq.com>
2018-03-25 09:33:37 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <imgs-list></imgs-list>
+  </div>
+</template>
+
+<script>
+import imgsList from './components/imgs-list';
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
+
+export default {
+  name: 'App',
+  components: {
+    imgsList,
+      PulseLoader
+  }
+}
+</script>
+
+<style>
+  .container {
+    height: 100%;
+    width: 100%;
+    position: absolute;
+  }
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,16 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import App from './App.vue';
+import infiniteScroll from 'vue-infinite-scroll';
+
+Vue.use(infiniteScroll);
+
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+});
